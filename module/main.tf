d1211a7b22919a9e1c0db7c22921f35efbbe0f69
lucardcoder <gokalpkocer@gmail.com>
2023-03-01 18:35:01 -0600

    pushed

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,9 @@
-resource "google_artifact_registry_repository" "my-repo" {
-  location      = var.artifact-config["location"]
-  repository_id = var.artifact-config["repository_id"]
-  description   = "Created by terraform"
-  format        = "DOCKER"
-}
 
+
+module "my-repo" {
+  source = "../"
+  artifact-config = {
+    repository_id = "my-repository"
+    location      = "us-central1"
+  }
+}
